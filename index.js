62de3aeaf36a1cc9a7ff4eb12910360637b968ee
lizzyt10h <lizzyt10h@gmail.com>
2018-04-20 09:02:56 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// ROUTES FOR testAPI
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
